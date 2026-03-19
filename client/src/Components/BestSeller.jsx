d1afac0971369cda9c107e57be13aa32e89a15b1
Swati Vagare <vagareswati50@gmail.com>
2026-03-19 14:09:18 +0530

    bestseller import fix

--- client/src/Components/BestSeller.jsx
+++ client/src/Components/BestSeller.jsx
@@ -11,7 +11,7 @@ const BestSeller = () => {
 
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md-gap-6 lg:grid-cols-5 mt-6'>
         {products.slice(0, 5).map((item) => (
-          <ProductCard key={item._id} product={item} />
+          <ProductCart key={item._id} product={item} />
         ))}
       </div>
     </div>
