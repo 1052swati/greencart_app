9b8d441d838260b6b3c9a4038dc33cc68ad539e7
Swati Vagare <vagareswati50@gmail.com>
2026-03-19 15:34:23 +0530

    login issue  fix

--- client/src/Context/AppContext.jsx
+++ client/src/Context/AppContext.jsx
@@ -82,7 +82,7 @@ export const AppContextProvider = ({ children }) => {
   // };
   const addToCart = (itemId) => {
 
-  if (!user || !user._id) {
+  if (!user) {
     toast.error("Please login first");
     return;
   }
